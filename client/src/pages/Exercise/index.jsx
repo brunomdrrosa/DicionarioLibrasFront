7d83dd94e323b66959758528170651fb3fdf6ea7
riandeoliveira <riandiasdeoliveira2001@gmail.com>
2022-07-05 15:05:59 -0300

    Criado recompensa de exercício

--- client/src/pages/Exercise/index.jsx
+++ client/src/pages/Exercise/index.jsx
@@ -1,21 +1,20 @@
-import styles from "./styles.module.scss";
-import { Helmet } from "react-helmet";
-import { ExerciseHeader } from "../../components/ExerciseHeader";
-import { GifCard } from "../../components/GifCard";
 import { useContext, useRef } from "react";
-import {
-  QuestionContext,
-  QuestionProvider,
-} from "../../contexts/QuestionContext";
 import { Button } from "react-bootstrap";
-import { QuestionResult } from "../../components/QuestionResult";
-import { useParams } from "react-router-dom";
-import courses from "../../data/courses.json";
+import { Helmet } from "react-helmet";
+import { useNavigate, useParams } from "react-router-dom";
 import right_answer from "../../assets/audios/right_answer.wav";
 import wrong_answer from "../../assets/audios/wrong_answer.wav";
+import { ExerciseHeader } from "../../components/ExerciseHeader";
+import { GifCard } from "../../components/GifCard";
+import { QuestionResult } from "../../components/QuestionResult";
+import { QuestionContext } from "../../contexts/QuestionContext";
+import courses from "../../data/courses.json";
+import styles from "./styles.module.scss";
 
 export const Exercise = ({ questionData }) => {
   const { course_name, unity_difficulty } = useParams();
+
+  const navigate = useNavigate();
 
   const course = courses.filter(({ path }) => path === course_name)[0];
   const unity = course.units.filter(({ path }) => path === unity_difficulty)[0];
@@ -54,6 +53,8 @@ export const Exercise = ({ questionData }) => {
     }
   };
 
+  const onFinishExercise = () => navigate("/aprender");
+
   const handleNextQuestion = () => {
     handlePercentage();
     handleIndex();
@@ -73,43 +74,63 @@ export const Exercise = ({ questionData }) => {
       </Helmet>
       <div className={styles.layout}>
         <ExerciseHeader />
-        <main className={styles.main}>
-          <section className={styles.container}>
-            <div className={styles.question_container}>
-              <div className={styles.question_content}>
-                <p className={styles.question}>
-                  Traduza a palavra:{" "}
-                  <span id="questionExercise" className={styles.word}>
-                    {unity.questions[index].word}
-                  </span>
-                </p>
+        {index < 5 ? (
+          <>
+            <main className={styles.main}>
+              <section className={styles.container}>
+                <div className={styles.question_container}>
+                  <div className={styles.question_content}>
+                    <p className={styles.question}>
+                      Traduza a palavra:{" "}
+                      <span id="questionExercise" className={styles.word}>
+                        {unity.questions[index].word}
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.gifs_container}>
+                  <div className={styles.gifs}>
+                    {unity.questions[index].answers.map((answer) => (
+                      <GifCard
+                        gif={answer.path}
+                        onClick={() =>
+                          onAlternativeSelected(answer.right_answer)
+                        }
+                      />
+                    ))}
+                  </div>
+                </div>
+              </section>
+            </main>
+            <footer className={styles.footer}>
+              <div className={styles.footer_container} ref={footerElement}>
+                <QuestionResult />
+                <Button
+                  primary
+                  className={styles.button}
+                  disabled={!isCorrectAnswer}
+                  onClick={handleNextQuestion}
+                >
+                  Próxima pergunta
+                </Button>
               </div>
-            </div>
-            <div className={styles.gifs_container}>
-              <div className={styles.gifs}>
-                {unity.questions[index].answers.map((answer) => (
-                  <GifCard
-                    gif={answer.path}
-                    onClick={() => onAlternativeSelected(answer.right_answer)}
-                  />
-                ))}
-              </div>
-            </div>
-          </section>
-        </main>
-        <footer className={styles.footer}>
-          <div className={styles.footer_container} ref={footerElement}>
-            <QuestionResult />
+            </footer>
+          </>
+        ) : (
+          <div className={styles.finish_container}>
+            <h2>PARABÉNS! Você completou o exercício</h2>
+            <p>
+              Você recebeu <strong>5 pontos de XP!</strong>
+            </p>
             <Button
               primary
               className={styles.button}
-              disabled={!isCorrectAnswer}
-              onClick={handleNextQuestion}
+              onClick={onFinishExercise}
             >
-              Próxima pergunta
+              Sair
             </Button>
           </div>
-        </footer>
+        )}
       </div>
     </>
   );
