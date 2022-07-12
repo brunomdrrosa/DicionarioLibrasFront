bb58849edd99f5ae19a897d1e1f9414a9fce5b18
brunomdr <brunomdr46@gmail.com>
2022-07-11 22:07:15 -0300

    Commit inicial

---
+++ backend/src/main/java/com/cesuca/glossarioLibras/GlossarioLibrasApplication.java
@@ -0,0 +1,13 @@
+package com.cesuca.glossarioLibras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlossarioLibrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlossarioLibrasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cesuca/glossarioLibras/GlossarioLibrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesuca.glossarioLibras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlossarioLibrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
